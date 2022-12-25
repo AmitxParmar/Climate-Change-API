c41d3131f15fa42add553113b501addca9fb35ef
Amit Parmar <amitparmar901@gmail.com>
2022-12-26 00:04:24 +0530

    add news route

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const PORT = 3306;
 import express from 'express';
 import axios from 'axios';
 import cheerio from 'cheerio';
-
 const app = express();
+
+app.get("/news", (req, res) => {
+res.json("WElcome to climate change api")
+}
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
